f284ca0cd1b685c3ac031ea8313e0239b3f84182
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-08-04 10:06:25 +0300

    initial commit

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
-// Code EyesOnMe Component Here
+// components/EyesOnMe.js
+import React from 'react';
+
+function EyesOnMe() {
+  // Event handler for when the button gains focus
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  // Event handler for when the button loses focus
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button 
+      onFocus={handleFocus}  // Attach focus event handler
+      onBlur={handleBlur}    // Attach blur event handler
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
